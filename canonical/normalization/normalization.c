858bcae5709263fe77aa88abd394307fd52d17a1
gokulkrishnaks <gokulkrishnaks2003@gmail.com>
2026-05-31 23:03:06 +0530

    canonical/normalization: add normalization header and implementation

--- canonical/normalization/normalization.c
+++ canonical/normalization/normalization.c
@@ -1,6 +1,16 @@
 
 #include "canonical/normalization/normalization.h"
+#include <time.h>
+
+uint64_t current_us(void){
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC); 
+    return (uint64_t)ts.tv_sec * 1000000ULL + ts.tv_nsec / 1000ULL;
+}
+
 
 int stich_normalize(stich_frame_t *frame){
-   // if (frame->)
+    frame->timestamp_us = current_us();
+    
+    return(frame->state = STICH_FRAME_STATE_CANONICAL);
 }
